3f467c71bec94ea53f2ef9c8b58bae2e50cbe783
Kien Truong <duckientruong@gmail.com>
2014-05-01 20:59:13 +0700

    Add logging when a type of request is not supported

--- src/main/scala/com/dikei/Forwarder.scala
+++ src/main/scala/com/dikei/Forwarder.scala
@@ -9,6 +9,7 @@ import scala.util.{Failure, Success, Try}
 import dispatch._, Defaults._
 import org.apache.http.message.BasicHttpResponse
 import scala.collection.JavaConversions._
+import org.apache.http.{HttpRequest, Header, RequestLine}
 
 case object StartProcessing
 
@@ -44,55 +45,68 @@ class Forwarder(val clientSocket: Socket) extends Actor{
       parser.parse()
     }
 
-    val writer = Try {
-      writerFactory.create(outBuffer)
-    }
-
     request match {
       case Success(r) =>
         val headers = r.getAllHeaders
         val requestLine = r.getRequestLine
+        val method = requestLine.getMethod.toLowerCase
 
-        val request = url(requestLine.getUri)
-        for (header <- headers) {
-          request.setHeader(header.getName, header.getValue)
-          request.setMethod(requestLine.getMethod)
-        }
-        request.setFollowRedirects(followRedirects = true)
-
-        logger.info("Downloading: {}", requestLine.getUri)
-        val result = Http(request)
-
-        result.onComplete {
-          case Success(resp) =>
-            logger.info("Download completed: {}", resp.getUri)
-
-            val response = new BasicHttpResponse(requestLine.getProtocolVersion, resp.getStatusCode, resp.getStatusText)
-            for {
-              key <- resp.getHeaders.keySet
-              value = resp.getHeaders.getFirstValue(key)
-            } {
-              response.setHeader(key, value)
-            }
-
-            writer.foreach { w =>
-              //Write the header
-              w.write(response)
-              //Write the body of the response
-              outBuffer.write(resp.getResponseBodyAsBytes)
-              context.stop(self)
-            }
-          case Failure(e) =>
-            writer.foreach { w =>
-              w.write(new BasicHttpResponse(requestLine.getProtocolVersion, 500, "Unable to proxy request"))
-            }
+        logger.info("Request method: {}", method)
+        method match {
+          case "get" =>
+            getProxy(requestLine, headers)
+          case _ =>
+            logger.info("Unsupported method: {}", method)
             context.stop(self)
         }
+
       case Failure(e) =>
         logger.debug("Invalid request")
         context.stop(self)
     }
 
+  }
+
+  private def getProxy(requestLine: RequestLine, headers: Array[Header]) {
+    val writer = Try {
+      writerFactory.create(outBuffer)
+    }
+
+    val request = url(requestLine.getUri)
+    for (header <- headers) {
+      request.setHeader(header.getName, header.getValue)
+      request.setMethod(requestLine.getMethod)
+    }
+    request.setFollowRedirects(followRedirects = true)
+
+    logger.info("Downloading: {}", requestLine.getUri)
+    val result = Http(request)
+
+    result.onComplete {
+      case Success(resp) =>
+        logger.info("Download completed: {}", resp.getUri)
+
+        val response = new BasicHttpResponse(requestLine.getProtocolVersion, resp.getStatusCode, resp.getStatusText)
+        for {
+          key <- resp.getHeaders.keySet
+          value = resp.getHeaders.getFirstValue(key)
+        } {
+          response.setHeader(key, value)
+        }
+
+        writer.foreach { w =>
+          //Write the header
+          w.write(response)
+          //Write the body of the response
+          outBuffer.write(resp.getResponseBodyAsBytes)
+          context.stop(self)
+        }
+      case Failure(e) =>
+        writer.foreach { w =>
+          w.write(new BasicHttpResponse(requestLine.getProtocolVersion, 500, "Unable to proxy request"))
+        }
+        context.stop(self)
+    }
   }
 
   override def postStop() {
